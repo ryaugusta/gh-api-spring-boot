f1c054e716522e108754e14acb3ab34f5ba4682b
ryan-augusta_boxboat <ryan.augusta@ibm.com>
2024-11-04 15:59:07 -0500

    OpenAPI generated spec for Swagger UI

--- src/main/java/githubapiserver/Repository.java
+++ src/main/java/githubapiserver/Repository.java
@@ -1,32 +1,9 @@
 package githubapiserver;
 
+import lombok.Data;
+@Data
 public class Repository {
     private String org;
     private String name;
     private String visibility;
-
-    // Getters and Setters
-    public String getOrg() {
-        return org;
-    }
-
-    public void setOrg(String org) {
-        this.org = org;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(String visibility) {
-        this.visibility = visibility;
-    }
 }
